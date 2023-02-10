614debf96a303c26cd51bc6c17c79132b725c8b6
justinraj89 <rajewskijustin@gmail.com>
2023-02-09 18:27:31 -0800

    title added to header

--- app/Header.js
+++ app/Header.js
@@ -8,6 +8,7 @@ function Header() {
   return (
     <header className="flex justify-between pt-4">
         <BookOpenIcon className="h-8 w-8 ml-4 text-gray-500"/>
+        <div className="font-serif text-2xl text-gray-500 dark:text-gray-100 font-semibold tracking-widest">Dictionary</div>
         <DarkModeButton />
     </header>
   )
