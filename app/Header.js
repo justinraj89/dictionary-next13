9c659aee213ed96c9259dbc336d8f0c18bc21809
justinraj89 <rajewskijustin@gmail.com>
2023-02-09 17:06:10 -0800

    header/darkmode

---
+++ app/Header.js
@@ -0,0 +1,13 @@
+import DarkModeButton from "./DarkModeButton"
+import { BookOpenIcon } from "@heroicons/react/24/outline";
+
+function Header() {
+  return (
+    <header className="flex justify-between pt-4">
+        <BookOpenIcon className="h-8 w-8 ml-4"/>
+        <DarkModeButton />
+    </header>
+  )
+}
+
+export default Header
